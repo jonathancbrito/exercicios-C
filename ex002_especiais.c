4fa34f59839c5363aaf5e278d536f25dfc86f9c0
Jonathan Brito <jonathan.brito.15@protonmail.com>
2021-05-26 22:08:28 -0300

    Create ex002_especiais.c

---
+++ ex002_especiais.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+void main(){
+    setlocale(LC_ALL, "Portuguese");
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-");
+    printf("\n \\a = \tBeep");
+    printf("\n \\n = \tNova Linha");
+    printf("\n \\t = \tTabulação");
+    printf("\n \\ = \tBarra");
+    printf("\n %% = \tPorcentagem");
+    printf("\n \? = Interrogação");
+    printf("\n=-=-=-=-=-=-=-=-=-=-=-=-");
+
+}
