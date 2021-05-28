fdf3727726f31997c3687c879b7419aed93622c3
Jonathan Brito <jonathan.brito.15@protonmail.com>
2021-05-28 12:30:07 -0300

    Update ex007_dobro_e_terca_partet_.c

--- ex007_dobro_e_terca_partet_.c
+++ ex007_dobro_e_terca_partet_.c
@@ -6,8 +6,8 @@ void main() {
     float terpt;
     setlocale(LC_ALL, "Portuguese");
     printf("Digite um número: ");
-    scanf("%d", &num);
+    scanf("%i", &num);
     dobro = 2*num;
-    terpt = num/3;
-    printf("\nAnalisando o número %d, seu dobro é %d e a terça parte é %.1f.", num, dobro, terpt);
+    terpt = (float)num/3; // (float)num para divisão real
+     printf("\nAnalisando o número %d, seu dobro é %d e a terça parte é %.2f.", num, dobro, terpt);
 }
