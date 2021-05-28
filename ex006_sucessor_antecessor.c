fa2d1e4ff6c46d0673d750aec897254c7156dbf0
Jonathan Brito <jonathan.brito.15@protonmail.com>
2021-05-28 12:06:44 -0300

    Create ex006_sucessor_antecessor.c

---
+++ ex006_sucessor_antecessor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main() {
+    setlocale(LC_ALL, "Portuguese");
+    int num, suces, antec;
+    printf("Digite um número para encontrar o sucessor e o antecessor: ");
+    scanf("%d", &num);
+    suces = num+1;
+    antec = num-1;
+    printf("\nO sucessor de %d é %d e seu antecessor é %d", num, suces, antec);
+}
