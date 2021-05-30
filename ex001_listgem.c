fd04eb9b4407a7f4376d3e63c1d4a14da532b6d5
Jonathan Brito <jonathan.brito.15@protonmail.com>
2021-05-30 20:35:20 -0300

    Update ex001_listgem.c

--- ex001_listgem.c
+++ ex001_listgem.c
@@ -3,11 +3,11 @@
 
 void main(){
     setlocale(LC_ALL, "Portuguese");
-    printf("Linstagem de Alunos");
+    printf("Listagem de Alunos");
     printf("\nNome    \tNota");
     printf("\n--------------------");
     printf("\nAna Beatriz \t8.5");
     printf("\nBianca Martins \t9.0");
-    printf("\nCláudio Sá \t5.5");
+    printf("\nClï¿½udio Sï¿½ \t5.5");
     printf("\nGiovana Silva \t7.5");
 }
