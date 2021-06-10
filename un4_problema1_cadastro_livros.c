d753bb0de0a7104a34e9e9c653e6d666adf69ec7
Jonathan Brito <jonathan.brito.15@protonmail.com>
2021-06-09 23:17:37 -0300

    Update un4_problema1_cadastro_livros.c

--- un4_problema1_cadastro_livros.c
+++ un4_problema1_cadastro_livros.c
@@ -3,6 +3,8 @@
 #include <locale.h>
 #include <string.h>
 
+#define TAM 3
+
 // registro para cadastro dos livros
 struct livros {
     int codigo, ano;
@@ -12,22 +14,23 @@ struct livros {
 int main() {
     setlocale(LC_ALL, "Portuguese");
     struct livros info[20];
-    int i;
-    int option;
+    struct livros troca;
+    int i, j;
+    int option, acha, busca;
 
-    // Menu
-    printf("-----------------------------------------\n");
-    printf("      <<< CADASTRO DE LIVROS >>>\n");
-    printf("-----------------------------------------\n\n");
-    printf("Escolha uma opção abaixo:\n");
-    printf("1 - CADASTRAR LIVROS\n2 - IMPRIMIR INFORMAÇÕES DOS LIVROS\n3 - PESQUISAR LIVROS POR CÓDIGOS\n4 - ORDENAR OS LIVROS POR ANO\n5 - SAIR DO PROGRAMA\n");
-    scanf("%d", &option);
-
-    while(option >=1 && option <= 5 || option < 1 || option > 5) {
+    option = 0;
+    while(option != 5) {
+        // Menu
+        printf("-----------------------------------------\n");
+        printf("      <<< CADASTRO DE LIVROS >>>\n");
+        printf("-----------------------------------------\n\n");
+        printf("Escolha uma opção abaixo:\n");
+        printf("1 - CADASTRAR LIVROS\n2 - IMPRIMIR INFORMAÇÕES DOS LIVROS\n3 - PESQUISAR LIVROS POR CÓDIGOS\n4 - ORDENAR OS LIVROS POR ANO\n5 - SAIR DO PROGRAMA\n");
+        scanf("%d", &option);
 
         if (option == 1) {
-
-           for (i = 0; i < 3; i++) {
+           system("cls");
+           for (i = 0; i < TAM; i++) {
               printf("Digite o código do livro: ");
               fflush(stdin);
               scanf("%d", info[i].codigo);
@@ -50,10 +53,10 @@ int main() {
             }
 
         } else if (option == 2) {
-            for (i = 0; i < 2; i++) {
-              system("cls");
-
+            system("cls");
+            for (i = 0; i < TAM; i++) {
               printf("\n\nLIVRO %d\n", i+1);
+              printf("\nCódigo: %d\n", info[i].codigo);
               printf("Título: %s\n", info[i].titulo);
               printf("Autor: %s\n", info[i].autor);
               printf("Área: %s\n", info[i].area);
@@ -68,27 +71,44 @@ int main() {
            
 
         } else if (option == 3) {
-            printf("ok\n");
-            system("pause");
             system("cls");
-            // volta para o Menu
-            main();
+            printf("Digite o que deseja buscar: ");
+            fflush(stdin);
+            scanf("%d", &busca);
+            i = 0;
+            acha = 0;
+            while ((i < TAM) && (acha == 0)) {
+                if (info[i].codigo == busca) {
+                    acha = 1;
+                } else {
+                    i++; 
+                } if (acha == 1) {
+                    printf("\nCÓDIGO: %d\n", info[i].codigo);
+                    printf("TÍTULO: %s\n", info[i].titulo);
+                    printf("AUTOR: %s\n", info[i].autor);
+                    printf("ÁREA: %s\n", info[i].area);
+                    printf("ANO: %d\n", info[i].ano);
+                    printf("EDITORA: %s\n\n", info[i].editora);
+                } else if (acha != 1){
+                     printf("\nRegistro não encontrado!.");
 
+                } else if (option == 4) {
+                    system("cls");
+                    for (i=0;i<TAM-1;i++) {
+                        for (j=1;j<TAM;j++){
+                            if (info[i].ano > info[i].ano){
+                                troca = info[i];
+                                info[i] = info[j];
+                                info[j] = troca;
+                            }
 
-        } else if (option == 4) {
-            printf("ok");
-            system("pause");
-            system("cls");
-            main();
-        } else if (option == 5) {
-            exit(0);
-        } else if (option < 1 || option > 5){
-            printf("Opção inválida!\n");
-            system("pause");
-            system("cls");
-            main();
-        }
-    
+                        }
+                    }
+                } for (i=0;i<TAM;i++){
+                    printf("\nCÓDIGO: %d, TÍTULO: %s, ANO: %d", info[i].codigo, info[i].titulo, info[i].ano);
+                }
+            } 
+        }  
     }
     return (0);
 
